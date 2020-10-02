2bffa01e568031a8af84f106de5da8d52211f34b
rahul-singha <72270420+rahul-singha@users.noreply.github.com>
2020-10-02 21:40:45 +0530

    Add webpage

---
+++ script.js
@@ -0,0 +1,13 @@
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  autoPlay: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.carousel-cell', {
+  // options
+});
